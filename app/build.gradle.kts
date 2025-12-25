743338de0d42f15607bc2c9d8b3a66551f35a16f
Your Name <you@example.com>
2025-12-26 07:49:37 +0800

    不再使用广告

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.yourstudio.hskstroke.bishun"
         minSdk = 24
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.0.6"
+        versionCode = 8
+        versionName = "1.0.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
